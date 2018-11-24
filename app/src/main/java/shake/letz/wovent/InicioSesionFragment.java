ae08cdde877986b98ea0ec751e21eafe28f3c996
omuga <Obriel.muga.14@sansano.usm.cl>
2018-11-24 15:50:06 -0300

    First commit

---
+++ app/src/main/java/shake/letz/wovent/InicioSesionFragment.java
@@ -0,0 +1,178 @@
+package shake.letz.wovent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link InicioSesionFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link InicioSesionFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class InicioSesionFragment extends Fragment implements View.OnClickListener{
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    EditText et_login_email;
+    EditText et_login_password;
+    Button btn_login;
+    TextView link_signup;
+    FirebaseAuth firebaseAuth;
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public InicioSesionFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment InicioSesionFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static InicioSesionFragment newInstance(String param1, String param2) {
+        InicioSesionFragment fragment = new InicioSesionFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_inicio_sesion, container, false);
+        firebaseAuth = FirebaseAuth.getInstance();
+        btn_login = (Button) v.findViewById(R.id.btn_login);
+        et_login_email  = (EditText) v.findViewById(R.id.et_login_email);
+        et_login_password = (EditText) v.findViewById(R.id.et_login_password);
+        btn_login.setOnClickListener(this);
+
+        //Link para ir a layout de registro
+        link_signup = (TextView)v.findViewById(R.id.link_signup);
+        link_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction fr = getFragmentManager().beginTransaction();
+                fr.replace(R.id.fragment_login,new RegistroFragment());
+                fr.commit();
+            }
+        });
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void LoginUsuario(){
+        String email = et_login_email.getText().toString().trim();
+        String password = et_login_password.getText().toString().trim();
+        if (TextUtils.isEmpty(email)){
+            Toast.makeText(getContext(),"Ingrese un Email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            Toast.makeText(getContext(), "Ingrese una Contraseña", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getContext(), "Inicio de sesión satisfactorio", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getContext(), UserProfileActivity.class));
+                }
+                else{
+                    FirebaseAuthException e = (FirebaseAuthException )task.getException();
+                    Toast.makeText(getContext(), "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login:
+                LoginUsuario();
+                break;
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
