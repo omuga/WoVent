71a0d65c117c7343dec55606b86b4ddae1caf7d9
civky <gladys.gonzalez.14@sansano.usm.cl>
2018-11-28 17:53:32 -0300

    Vista detalle (en progreso)

---
+++ app/src/main/java/shake/letz/wovent/DetalleFragment.java
@@ -0,0 +1,25 @@
+package shake.letz.wovent;
+import android.content.Intent;
+import android.os.Parcelable;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import Objetos.Evento;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class DetalleFragment {
+}
